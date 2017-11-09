f7805f70d6782428e2f525a889ec8975913f4da8
yiwen117 <563262161@qq.com>
2017-11-09 16:41:16 +0800

    创建类

---
+++ java-test/src/main/java/com/i2f/test/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.i2f.test;
+
+import org.junit.Test;
+
+public class HelloWorld {
+
+	public static void main(String[] args) throws Exception {
+		testName();
+		System.out.println("hello world ?");
+	}
+	
+	@Test
+	public static void testName() throws Exception {
+		
+		System.out.println("fhskfurkhfkdfiekfu");
+	}
+}
